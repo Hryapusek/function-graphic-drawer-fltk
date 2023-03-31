9352fb1332d2a2839977821ceef658488d279fe3
Александр Абраамян <samvelcc25@outlook.com>
2023-03-31 19:55:18 +0300

    update: x axis unit intervals
    Now positive x unit intervals are drawn correctly with values.

--- FunctionGraphWindow.cxx
+++ FunctionGraphWindow.cxx
@@ -21,7 +21,7 @@ void FunctionGraphWindow::draw()
 {
   Fl_Double_Window::draw();
   drawXyAxis();
-  drawUnitInterval();
+  drawXUnitInterval();
   //drawFunction();
 }
 
@@ -45,9 +45,9 @@ void FunctionGraphWindow::drawXyAxis()
     const char *xAxisText = "X";
     fl_measure(xAxisText, xStrWidth, xStrHeight);
     if (origin.y < h() / 2)
-      fl_draw(xAxisText, w() - xStrWidth - 5, origin.y + xStrHeight + 5);
+      fl_draw(xAxisText, w() - xStrWidth - 5, origin.y + xStrHeight + 30);
     else
-      fl_draw(xAxisText, w() - xStrWidth - 5, origin.y - xStrHeight - 5);
+      fl_draw(xAxisText, w() - xStrWidth - 5, origin.y - xStrHeight - 30);
   }
 
   //drawing Y axis
@@ -69,56 +69,58 @@ void FunctionGraphWindow::drawXyAxis()
 
 }
 
-void FunctionGraphWindow::drawUnitInterval()
+void FunctionGraphWindow::drawXUnitInterval()
 {
-
-  //int nUnits = (w() / 2 - 10) / unitWidthInPixels;
-  int nUnits = 10;
-  int unitWidthInPixels = (w() / 2 - 20) / nUnits;
+  Pixel origin = getOrigin();
+  int nUnits = 12;
+  int nNegUnits = (double)origin.x / w() * nUnits;
+  int nPosUnits = nUnits - nNegUnits;
   int lineWidth = 2;
   int lineStyle = FL_SOLID;
   Fl_Color lineColor = FL_WHITE;
-  int lineLengthHalf = 3;
-  int yUpperBound = h() / 2 - lineLengthHalf;
-  int yLowerBound = h() / 2 + lineLengthHalf;
-  for (int i = 1; i <= nUnits; ++i)
+  int lineLengthHalf = 6;
+  int yUpperBound = origin.y - lineLengthHalf;
+  int yLowerBound = origin.y + lineLengthHalf;
+  if (nPosUnits != 0) //place only positive unit intervals
   {
-    int posUnitOffset = w() / 2 + i * unitWidthInPixels;
-    int negUnitOffset = w() / 2 - i * unitWidthInPixels;
-    fl_line_style(lineStyle, lineWidth);
-    fl_color(lineColor);
-    fl_yxline(posUnitOffset, yUpperBound, yLowerBound);
-
-    fl_line_style(lineStyle, lineWidth);
-    fl_color(lineColor);
-    fl_yxline(negUnitOffset, yUpperBound, yLowerBound);
-
+    //width in pixels of single unit interval;
+    int stepInPixels = (w() - origin.x - 10) / nPosUnits;
+    //value of single unit interval
+    double stepValue = (xRight - std::max(0., xLeft)) / nPosUnits;
+    int firstUnitOffset;
+    double firstUnitVal;
+    if (xLeft <= 0)
     {
-      Fl_Font font = FL_TIMES;
-      int fontSize = 13;
-      Fl_Color fontColor = FL_WHITE;
-      fl_font(font, fontSize);
-      fl_color(fontColor);
-      int strWidth, strHeight;
-      std::string posUnitValue = std::to_string(round((1. / nUnits * i) * 100.) / 100.);
-      posUnitValue.erase ( posUnitValue.find_last_not_of('0') + 1, std::string::npos );
-      posUnitValue.erase ( posUnitValue.find_last_not_of('.') + 1, std::string::npos );
-      fl_measure(posUnitValue.c_str(), strWidth, strHeight);
-      fl_draw(posUnitValue.c_str(), w() / 2 + i * unitWidthInPixels - strWidth / 2., yLowerBound + strHeight);
+      firstUnitOffset = origin.x + stepInPixels;
+      firstUnitVal = stepValue;
     }
-
+    else
     {
-      Fl_Font font = FL_TIMES;
-      int fontSize = 13;
-      Fl_Color fontColor = FL_WHITE;
-      fl_font(font, fontSize);
-      fl_color(fontColor);
-      int strWidth, strHeight;
-      std::string negUnitValue = std::to_string(-(round((1. / nUnits * i) * 100.) / 100.));
-      negUnitValue.erase(negUnitValue.find_last_not_of('0') + 1, std::string::npos );
-      negUnitValue.erase(negUnitValue.find_last_not_of('.') + 1, std::string::npos );
-      fl_measure(negUnitValue.c_str(), strWidth, strHeight);
-      fl_draw(negUnitValue.c_str(), w() / 2 - i * unitWidthInPixels - strWidth / 2., yLowerBound + strHeight);
+      firstUnitOffset = (1 - std::fmod(xLeft, stepValue) / stepValue) * stepInPixels;
+      firstUnitVal = xLeft + (1 - std::fmod(xLeft, stepValue) / stepValue) * stepValue;
+    }
+    for (int i = 0; i < nPosUnits; ++i)
+    {
+      int curUnitOffset = firstUnitOffset + i * stepInPixels;
+      fl_line_style(lineStyle, lineWidth);
+      fl_color(lineColor);
+      fl_yxline(curUnitOffset, yUpperBound, yLowerBound);
+      {
+        Fl_Font font = FL_TIMES;
+        int fontSize = 16;
+        Fl_Color fontColor = FL_WHITE;
+        fl_font(font, fontSize);
+        fl_color(fontColor);
+        int strWidth, strHeight;
+        std::string posUnitValue = std::to_string(round((firstUnitVal + stepValue * i) * 100.) / 100.);
+        posUnitValue.erase(posUnitValue.find_last_not_of('0') + 1, std::string::npos );
+        posUnitValue.erase(posUnitValue.find_last_not_of('.') + 1, std::string::npos );
+        fl_measure(posUnitValue.c_str(), strWidth, strHeight);
+        if (isYAxisInUpperHalf())
+          fl_draw(posUnitValue.c_str(), curUnitOffset - strWidth / 2., yLowerBound + strHeight);
+        else
+          fl_draw(posUnitValue.c_str(), curUnitOffset - strWidth / 2., yUpperBound - strHeight);
+      }
     }
   }
 }
@@ -140,6 +142,11 @@ FunctionGraphWindow::Pixel FunctionGraphWindow::getOrigin()
   else
     x = std::abs(xLeft / (xRight - xLeft) * w());
   return { x, y };
+}
+
+bool FunctionGraphWindow::isYAxisInUpperHalf()
+{
+  return getOrigin().y <= h() / 2;
 }
 
 /*void FunctionGraphWindow::drawFunction()
