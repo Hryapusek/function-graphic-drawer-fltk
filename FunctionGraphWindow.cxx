d2a39f575e312e5af0723b573301aaa75b154385
Александр Абраамян <samvelcc25@outlook.com>
2023-04-02 23:03:42 +0300

    new: both axis unit intervals are now drawn correctly
    Also a little change bug fix in drawing x axis unit intervals values

--- FunctionGraphWindow.cxx
+++ FunctionGraphWindow.cxx
@@ -4,6 +4,7 @@
 #include <string>
 #include <cmath>
 #include <iostream>
+#include <functional>
 
 FunctionGraphWindow::FunctionGraphWindow(double (*f)(double))
   : Fl_Double_Window(600, 600) {
@@ -22,6 +23,7 @@ void FunctionGraphWindow::draw()
   Fl_Double_Window::draw();
   drawXyAxis();
   drawXUnitInterval();
+  drawYUnitInterval();
   //drawFunction();
 }
 
@@ -37,8 +39,8 @@ int FunctionGraphWindow::handle(int e)
     y = Fl::event_y();
     break;
   case FL_DRAG:
-    double dx = (Fl::event_x() - x) / 200.;
-    double dy = (Fl::event_y() - y) / 200.;
+    double dx = (Fl::event_x() - x) / (w() / 3.);
+    double dy = (Fl::event_y() - y) / (h() / 3.);
     xLeft -= dx;
     xRight -= dx;
     yLow += dy;
@@ -87,8 +89,8 @@ void FunctionGraphWindow::drawXyAxis()
     int yStrWidth, yStrHeight;
     const char *yAxisText = "Y";
     fl_measure(yAxisText, yStrWidth, yStrHeight);
-    if (origin.x < w() / 2)
-      fl_draw(yAxisText, origin.x + yStrWidth + 5, yStrHeight);
+    if (origin.x <= w() / 2)
+      fl_draw(yAxisText, origin.x + 5, yStrHeight);
     else
       fl_draw(yAxisText, origin.x - yStrWidth - 5, yStrHeight);
   }
@@ -137,10 +139,10 @@ void FunctionGraphWindow::drawXUnitInterval()
         posUnitValue.erase(posUnitValue.find_last_not_of('0') + 1, std::string::npos );
         posUnitValue.erase(posUnitValue.find_last_not_of('.') + 1, std::string::npos );
         fl_measure(posUnitValue.c_str(), strWidth, strHeight);
-        if (isYAxisInUpperHalf())
-          fl_draw(posUnitValue.c_str(), unitOffset - strWidth / 2., yLowerBound + strHeight);
+        if (isXAxisInUpperHalf())
+          fl_draw(posUnitValue.c_str(), unitOffset - strWidth / 2., yLowerBound + strHeight + 5);
         else
-          fl_draw(posUnitValue.c_str(), unitOffset - strWidth / 2., yUpperBound - strHeight);
+          fl_draw(posUnitValue.c_str(), unitOffset - strWidth / 2., yUpperBound - 5);
       }
       unitValue += step;
     }
@@ -175,14 +177,105 @@ void FunctionGraphWindow::drawXUnitInterval()
         strUnitValue.erase(strUnitValue.find_last_not_of('0') + 1, std::string::npos );
         strUnitValue.erase(strUnitValue.find_last_not_of('.') + 1, std::string::npos );
         fl_measure(strUnitValue.c_str(), strWidth, strHeight);
-        if (isYAxisInUpperHalf())
+        if (isXAxisInUpperHalf())
           fl_draw(strUnitValue.c_str(), unitOffset - strWidth / 2., yLowerBound + strHeight);
         else
-          fl_draw(strUnitValue.c_str(), unitOffset - strWidth / 2., yUpperBound - strHeight);
+          fl_draw(strUnitValue.c_str(), unitOffset - strWidth / 2., yUpperBound - 5);
       }
       unitValue -= step;
     }
   }
+}
+
+void FunctionGraphWindow::drawYUnitInterval()
+{
+  Pixel origin = getOrigin();
+  const int stepWidth = 60;
+  const double valueOnPixel = (yHigh - yLow) / h();
+  const double step = stepWidth * valueOnPixel;
+  int lineWidth = 2;
+  int lineStyle = FL_SOLID;
+  Fl_Color lineColor = FL_WHITE;
+  int lineLengthHalf = 6;
+  int xLeftBound = origin.x - lineLengthHalf;
+  int xRightBound = origin.x + lineLengthHalf;
+  if (yHigh > 0)
+  { //positive half
+    int unitOffset;
+    double unitValue;
+    if (yLow <= 0)
+    {
+      unitOffset = origin.y - stepWidth;
+      unitValue = step;
+    }
+    else
+    {
+      unitOffset = h() - (1 - std::fmod(yLow, step) / step) * stepWidth;
+      unitValue = yLow + (1 - std::fmod(yLow, step) / step) * step;
+    }
+    for (; unitOffset > 0; unitOffset -= stepWidth)
+    {
+      fl_line_style(lineStyle, lineWidth);
+      fl_color(lineColor);
+      fl_xyline(xLeftBound, unitOffset, xRightBound);
+      {
+        Fl_Font font = FL_TIMES;
+        int fontSize = 16;
+        Fl_Color fontColor = FL_WHITE;
+        fl_font(font, fontSize);
+        fl_color(fontColor);
+        int strWidth, strHeight;
+        std::string strUnitValue = std::to_string(round((unitValue) * 100.) / 100.);
+        strUnitValue.erase(strUnitValue.find_last_not_of('0') + 1, std::string::npos );
+        strUnitValue.erase(strUnitValue.find_last_not_of('.') + 1, std::string::npos );
+        fl_measure(strUnitValue.c_str(), strWidth, strHeight);
+        if (isYAxisInLeftHalf())
+          fl_draw(strUnitValue.c_str(), xRightBound + 5, unitOffset + strHeight / 4.);
+        else
+          fl_draw(strUnitValue.c_str(), xLeftBound - strWidth - 5, unitOffset + strHeight / 4.);
+      }
+      unitValue += step;
+    }
+  }
+  if (yLow < 0)
+  { //negative half
+    int unitOffset;
+    double unitValue;
+    if (yHigh >= 0)
+    {
+      unitOffset = origin.y + stepWidth;
+      unitValue = -step;
+    }
+    else
+    {
+      unitOffset = (step + std::fmod(yHigh, step)) / step * stepWidth;
+      unitValue = yHigh - step - std::fmod(yHigh, step);
+    }
+    for (; unitOffset < h(); unitOffset += stepWidth)
+    {
+      fl_line_style(lineStyle, lineWidth);
+      fl_color(lineColor);
+      fl_xyline(xLeftBound, unitOffset, xRightBound);
+      {
+        Fl_Font font = FL_TIMES;
+        int fontSize = 16;
+        Fl_Color fontColor = FL_WHITE;
+        fl_font(font, fontSize);
+        fl_color(fontColor);
+        int strWidth, strHeight;
+        std::string strUnitValue = std::to_string(round((unitValue) * 100.) / 100.);
+        strUnitValue.erase(strUnitValue.find_last_not_of('0') + 1, std::string::npos );
+        strUnitValue.erase(strUnitValue.find_last_not_of('.') + 1, std::string::npos );
+        fl_measure(strUnitValue.c_str(), strWidth, strHeight);
+        if (isYAxisInLeftHalf())
+          fl_draw(strUnitValue.c_str(), xRightBound + 5, unitOffset + strHeight / 4.);
+        else
+          fl_draw(strUnitValue.c_str(), xLeftBound - strWidth - 5, unitOffset + strHeight / 4.);
+      }
+      unitValue -= step;
+    }
+  }
+
 }
 
 FunctionGraphWindow::Pixel FunctionGraphWindow::getOrigin()
@@ -204,9 +297,14 @@ FunctionGraphWindow::Pixel FunctionGraphWindow::getOrigin()
   return { x, y };
 }
 
-inline bool FunctionGraphWindow::isYAxisInUpperHalf()
+inline bool FunctionGraphWindow::isXAxisInUpperHalf()
 {
   return getOrigin().y <= h() / 2;
+}
+
+inline bool FunctionGraphWindow::isYAxisInLeftHalf()
+{
+  return getOrigin().x <= w() / 2;;
 }
 
 /*void FunctionGraphWindow::drawFunction()
